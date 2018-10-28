ff811891e664bd5859832adbab5dd2d15d3d9476
Chris Hendrick <chris.hendrick@gmail.com>
2018-10-28 16:24:50 -0400

    function intro

---
+++ function.py
@@ -0,0 +1,17 @@
+def add(x,y):
+    return x + y
+    
+
+output = add(3,4)
+
+print(output)
+
+word = "pen"
+print(word[::-1])
+
+def reverse(text):
+    return text[::-1]
+
+word = input("What's your word?:".strip())
+output = reverse(word)
+print(output)
